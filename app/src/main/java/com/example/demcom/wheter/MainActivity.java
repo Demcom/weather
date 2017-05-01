21dcc4f0fbda0eeabc85059911085f442d5072bc
Daniel E. Moreno Q <demcomx@gmail.com>
2017-04-30 21:10:44 -0700

    Added function for temperature monitoring

--- app/src/main/java/com/example/demcom/wheter/MainActivity.java
+++ app/src/main/java/com/example/demcom/wheter/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.demcom.wheter;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -14,13 +15,22 @@ import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity{
 
-    private TextView textView;
+    private TextView humidityTextView;
+    private TextView temperatureTextView;
+    private TextView dateTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textView = (TextView) findViewById(R.id.textView_temperature);
+        temperatureTextView = (TextView) findViewById(R.id.textView_temperature);
+        humidityTextView = (TextView) findViewById(R.id.textView_humidity);
+        dateTextView = (TextView) findViewById(R.id.textView_date);
+        getTemperatureFromAPI();
+
+    }
+
+    public void getTemperatureFromAPI(){
         HttpRequests.getInstance().getWeather(this, new HtttpRequestCallback() {
             @Override
             public void HttpCallback(String response, String error) {
@@ -30,16 +40,31 @@ public class MainActivity extends AppCompatActivity{
                     try {
                         JSONObject jsonObject = new JSONObject(response);
                         Weather weather = new Weather(jsonObject);
-                        Log.d("HOLA", String.valueOf(weather.getTemperature()));
+                        temperatureTextView.setText(String.valueOf(weather.getTemperature()) + " ºC");
+                        humidityTextView.setText(String.valueOf(weather.getHumidity()));
+                        dateTextView.setText(weather.getDate());
+                        Log.d("JSON", response);
                     }catch (JSONException err){
                         Log.e("ERROR JSON", err.getMessage());
                     }
                 }
+                startMonitoringTemperature();
             }
         });
-
     }
 
+    public void startMonitoringTemperature(){
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                getTemperatureFromAPI();
+            }
+        }, secondsFromMilliseconds(10));
+    }
 
+    public int secondsFromMilliseconds(int seconds){
+        return seconds * 1000;
+    }
 
 }
